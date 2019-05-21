874e24199604e0fd27ea28bdbc24c4b218b3e577
solgamd <solgamd@gmail.com>
2019-05-21 15:15:22 +0200

    Added App.jsx and index.js; functional component added

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// const App = (props) => {
+//     return <h1>{props.name}</h1>;
+// }
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "I'/m good."
+        }
+    };
+
+    render();
+
+}
+
+
+export default App;
+
